f638829a24537e0379305ca9511935730d903797
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-09-01 15:28:51 +0000

    minor style improvement

--- plugins/SVfitLikelihoodMEt.cc
+++ plugins/SVfitLikelihoodMEt.cc
@@ -41,15 +41,11 @@ SVfitLikelihoodMEt<T1,T2>::~SVfitLikelihoodMEt()
 template <typename T1, typename T2>
 bool SVfitLikelihoodMEt<T1,T2>::isFittedParameter(int index) const
 {
-  switch (index)  
-  {
-     case SVfitAlgorithm<T1, T2>::kLeg1thetaRest:
-     case SVfitAlgorithm<T1, T2>::kLeg1phiLab:
-     case SVfitAlgorithm<T1, T2>::kLeg2thetaRest:
-     case SVfitAlgorithm<T1, T2>::kLeg2phiLab:
-        return true;
-  }
-  return false;
+  if      ( index == SVfitAlgorithm<T1,T2>::kLeg1thetaRest ) return true;
+  else if ( index == SVfitAlgorithm<T1,T2>::kLeg1phiLab    ) return true;
+  else if ( index == SVfitAlgorithm<T1,T2>::kLeg2thetaRest ) return true;
+  else if ( index == SVfitAlgorithm<T1,T2>::kLeg2phiLab    ) return true;
+  else return false;
 }
 
 template <typename T1, typename T2>
