ade6c94e938efd16a710d6517ef835d2a03722a6
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-09-08 10:20:01 +0000

    added likelihoods for tau decays via rho, a1 mesons

--- plugins/SVfitTauLikelihoodPolarization.cc
+++ plugins/SVfitTauLikelihoodPolarization.cc
@@ -2,6 +2,7 @@
 
 #include "DataFormats/TauReco/interface/PFTauDecayMode.h"
 
+#include "TauAnalysis/CandidateTools/interface/SVfitVMlineShapeIntegrand.h"
 #include "TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h"
 #include "TauAnalysis/CandidateTools/interface/svFitAuxFunctions.h"
 
@@ -53,6 +54,16 @@ void normalizeMatrixRows(TMatrixD& matrix)
 
 SVfitTauLikelihoodPolarization::SVfitTauLikelihoodPolarization(const edm::ParameterSet& cfg)
   : SVfitLegLikelihoodPolarizationBase<pat::Tau>(cfg),
+    xVMrhoSigma_(0),
+    xVMrhoBias_(0),
+    xVMa1NeutralSigma_(0),
+    xVMa1NeutralBias_(0),
+    xVMa1ChargedSigma_(0),
+    xVMa1ChargedBias_(0),
+    rhoLpolLineShape_(0),
+    rhoTpolLineShape_(0),
+    a1LpolLineShape_(0),
+    a1TpolLineShape_(0),
     likelihoodPhaseSpace_(0)
 {
 //--- create list of supported decay modes
@@ -141,19 +152,63 @@ SVfitTauLikelihoodPolarization::SVfitTauLikelihoodPolarization(const edm::Parame
    
 //--- close file from which histogram was loaded
   delete file_mapRecToGenTauDecayModes;
+
+//--- initialize resolution and bias values for reconstructing
+//    momentum fraction x = E(dist. pion)/E(vector meson)
+//    of rho/a1 vector meson carried by "distinguishable" pion
+//   (computed in laboratory frame)
+  edm::ParameterSet cfgResolution = cfg.getParameter<edm::ParameterSet>("resolution");
+
+  xVMrhoSigma_ = new TFormula("xVMrhoSigma", cfgResolution.getParameter<std::string>("xVMrhoSigma").data());
+  xVMrhoBias_ = new TFormula("xVMrhoBias", cfgResolution.getParameter<std::string>("xVMrhoBias").data());
+  xVMa1NeutralSigma_ = new TFormula("xVMa1NeutralSigma", cfgResolution.getParameter<std::string>("xVMa1NeutralSigma").data());
+  xVMa1NeutralBias_ = new TFormula("xVMa1NeutralBias", cfgResolution.getParameter<std::string>("xVMa1NeutralBias").data());
+  xVMa1ChargedSigma_ = new TFormula("xVMa1ChargedSigma", cfgResolution.getParameter<std::string>("xVMa1ChargedSigma").data());
+  xVMa1ChargedBias_ = new TFormula("xVMa1ChargedBias", cfgResolution.getParameter<std::string>("xVMa1ChargedBias").data());
  
+//--- create auxiliary classes for computation of vector meson line-shape integrals
+  useCollApproxFormulas_ = cfg.getParameter<bool>("useCollApproxFormulas");
+
+  rhoLpolLineShape_ = new SVfitVMlineShapeIntegral(SVfitVMlineShapeIntegrand::kVMrho, 
+						   SVfitVMlineShapeIntegrand::kVMlongitudinalPol, useCollApproxFormulas_);
+  rhoTpolLineShape_ = new SVfitVMlineShapeIntegral(SVfitVMlineShapeIntegrand::kVMrho, 
+						   SVfitVMlineShapeIntegrand::kVMtransversePol, useCollApproxFormulas_);
+  a1LpolLineShape_  = new SVfitVMlineShapeIntegral(SVfitVMlineShapeIntegrand::kVMa1, 
+						   SVfitVMlineShapeIntegrand::kVMlongitudinalPol, useCollApproxFormulas_);
+  a1TpolLineShape_  = new SVfitVMlineShapeIntegral(SVfitVMlineShapeIntegrand::kVMa1, 
+						   SVfitVMlineShapeIntegrand::kVMtransversePol, useCollApproxFormulas_);
+
 //--- generic "phase-space" plugin to be used for computing likelihood 
 //    for hadronic tau decays not in the list of decay modes 
 //    supported by the SVfitTauLikelihoodPolarization plugin
   edm::ParameterSet cfgLikelihoodPhaseSpace;
   typedef edmplugin::PluginFactory<SVfitLegLikelihoodBase<pat::Tau>* (const edm::ParameterSet&)> SVfitTauLikelihoodPluginFactory;
   likelihoodPhaseSpace_ = SVfitTauLikelihoodPluginFactory::get()->create("SVfitTauLikelihoodPhaseSpace", cfgLikelihoodPhaseSpace);
-  
-  useCollApproxFormulas_ = cfg.getParameter<bool>("useCollApproxFormulas");
+
+//--- initialize temporary variables,
+//    defined to speed-up computations
+  a1posMassTerm_ = (a1MesonMass2 + rhoMesonMass2)/(a1MesonMass*rhoMesonMass);
+  a1posMassTerm2_ = square(a1posMassTerm_);
+  a1negMassTerm_ = (a1MesonMass2 - rhoMesonMass2)/(a1MesonMass*rhoMesonMass);
+  a1q_ = 0.5*TMath::Sqrt(a1MesonMass2 - 4.*chargedPionMass2);
+  a1_8piDiv9_ = 8.*TMath::Pi()/9.;
+  a1_16piDiv9_ = 2.*a1_8piDiv9_;
 }
 
 SVfitTauLikelihoodPolarization::~SVfitTauLikelihoodPolarization()
 {
+  delete xVMrhoSigma_;
+  delete xVMrhoBias_;
+  delete xVMa1NeutralSigma_;
+  delete xVMa1NeutralBias_;
+  delete xVMa1ChargedSigma_;
+  delete xVMa1ChargedBias_;
+
+  delete rhoLpolLineShape_;
+  delete rhoTpolLineShape_;
+  delete a1LpolLineShape_;
+  delete a1TpolLineShape_;
+
   delete likelihoodPhaseSpace_;
 }
 
@@ -191,13 +246,13 @@ double SVfitTauLikelihoodPolarization::negLogLikelihoodPolarized(
   for ( size_t iDecayMode = 0; iDecayMode < numSupportedTauDecayModes_; ++iDecayMode ) {
     double probDecayMode = 0.;
     if      ( supportedTauDecayModes_[iDecayMode] == reco::PFTauDecayMode::tauDecay1ChargedPion0PiZero ) 
-      probDecayMode = probOneProngZeroPi0(tau, solution, tauLeptonPol);
+      probDecayMode = probOneProngZeroPi0s(tau, solution, tauLeptonPol);
     else if ( supportedTauDecayModes_[iDecayMode] == reco::PFTauDecayMode::tauDecay1ChargedPion1PiZero )
       probDecayMode = probOneProngOnePi0(tau, solution, tauLeptonPol);
     else if ( supportedTauDecayModes_[iDecayMode] == reco::PFTauDecayMode::tauDecay1ChargedPion1PiZero )
-      probDecayMode = probOneProngTwoPi0(tau, solution, tauLeptonPol);
+      probDecayMode = probOneProngTwoPi0s(tau, solution, tauLeptonPol);
     else if ( supportedTauDecayModes_[iDecayMode] == reco::PFTauDecayMode::tauDecay3ChargedPion0PiZero )
-      probDecayMode = probThreeProngZeroPi0(tau, solution, tauLeptonPol);
+      probDecayMode = probThreeProngZeroPi0s(tau, solution, tauLeptonPol);
     else 
       probDecayMode = probOtherDecayMode(tau, solution, tauLeptonPol);
     vProb_(iDecayMode) = probDecayMode;
@@ -222,10 +277,10 @@ double SVfitTauLikelihoodPolarization::negLogLikelihoodPolarized(
 //-------------------------------------------------------------------------------
 //
 
-double SVfitTauLikelihoodPolarization::probOneProngZeroPi0(
+double SVfitTauLikelihoodPolarization::probOneProngZeroPi0s(
          const pat::Tau& tau, const SVfitLegSolution& solution, double tauLeptonPol) const
 {
-  std::cout << "<SVfitTauLikelihoodPolarization::probOneProngZeroPi0>:" << std::endl;
+  std::cout << "<SVfitTauLikelihoodPolarization::probOneProngZeroPi0s>:" << std::endl;
           
   double prob = 0.;
 
@@ -247,24 +302,177 @@ double SVfitTauLikelihoodPolarization::probOneProngZeroPi0(
 double SVfitTauLikelihoodPolarization::probOneProngOnePi0(
          const pat::Tau& tau, const SVfitLegSolution& solution, double tauLeptonPol) const
 {
+//--- compute likelihood for tau- --> rho- nu --> pi- pi0 nu decay
 
+  std::cout << "<SVfitTauLikelihoodPolarization::probOneProngOnePi0>:" << std::endl;
 
-  // dummy implementation
-  return 0.;
+  double cosTheta = solution.cosThetaRest();
+  double theta = TMath::ACos(cosTheta);
+  double z = tau.energy()/solution.p4().energy();
+  
+  double probTauDecayL = (*rhoLpolLineShape_)(theta, tauLeptonPol, z);
+  double probTauDecayT = (*rhoTpolLineShape_)(theta, tauLeptonPol, z);
+  std::cout << " probTauDecayL = " << probTauDecayL << ", probTauDecayT = " << probTauDecayT << std::endl;
+
+//--- find "distinguishable" pion in tau-jet;
+//    in case "distinguishable" pion cannot be found 
+//   (e.g. in case "wrong" tau decay mode is reconstructed),
+//    assume the "distinguishable" pion to be very soft
+  const reco::Candidate* distPion = getDistPion(tau);
+  double xMeasured = ( distPion != 0 ) ? distPion->energy()/tau.energy() : 0.;
+  double thetaVMrho = solution.thetaVMrho();
+  double cosThetaVMrho = TMath::Cos(thetaVMrho);
+  double sinThetaVMrho = TMath::Sin(thetaVMrho);
+  double xFitted = 0.5*(1. + TMath::Sqrt(1 - 4.*(chargedPionMass2/rhoMesonMass2))*cosThetaVMrho); // [2], formula (41)
+
+  double probVMrhoDecayL, probVMrhoDecayT;
+  if ( !useCollApproxFormulas_ ) {
+    probVMrhoDecayL = 1.5*square(cosThetaVMrho)*sinThetaVMrho; // [2], formula (39)
+    probVMrhoDecayT = 0.75*cube(sinThetaVMrho);                // [2], formula (40)
+  } else {
+    probVMrhoDecayL = 1.5*(2*xFitted - 1.);                    // [2], formula (39)
+    probVMrhoDecayT = 3*xFitted*(1. - xFitted) ;               // [2], formula (40)
+  }
+
+  std::cout << " probVMrhoDecayL = " << probVMrhoDecayL << ", probVMrhoDecayT = " << probVMrhoDecayT << std::endl;
+
+  double xSigma = xVMrhoSigma_->Eval(tau.pt());
+  double xBias = xVMrhoBias_->Eval(tau.pt());
+  std::cout << " xSigma = " << xSigma << ", xBias = " << xBias << std::endl;
+  double probSmear = TMath::Gaus(xMeasured, xFitted + xBias, xSigma);
+  std::cout << " probSmear = " << probSmear << std::endl;
+
+  double probL = probTauDecayL*probVMrhoDecayL*probSmear;
+  double probT = probTauDecayT*probVMrhoDecayT*probSmear;
+  std::cout << "--> probL = " << probL << ", probT = " << probT << std::endl;
+
+  return (probL + probT);
 }
 
-double SVfitTauLikelihoodPolarization::probOneProngTwoPi0(
-         const pat::Tau& tau, const SVfitLegSolution& solution, double tauLeptonPol) const
+double SVfitTauLikelihoodPolarization::compVMa1x(
+         double cosThetaVMa1, double sinThetaVMa1, double cosThetaVMa1r, double sinThetaVMa1r, double cosPhiVMa1r) const
 {
-  // dummy implementation
-  return 0.;
+  return (1./a1MesonMass)*(0.25*rhoMesonMass*(a1posMassTerm_ + a1negMassTerm_*cosThetaVMa1) 
+                          + 0.5*a1q_*(a1posMassTerm_*cosThetaVMa1*cosThetaVMa1r + a1negMassTerm_*cosThetaVMa1r 
+				     - 2.*sinThetaVMa1*sinThetaVMa1r*cosPhiVMa1r)); // [2], formula (48)
 }
 
-double SVfitTauLikelihoodPolarization::probThreeProngZeroPi0(
+double SVfitTauLikelihoodPolarization::compVMa1DecayProbL(
+	 double cosThetaVMa1, double sinThetaVMa1, double cosThetaVMa1r, double sinThetaVMa1r, double cosPhiVMa1r) const
+{
+  return (square(a1posMassTerm_*cosThetaVMa1*cosThetaVMa1r - 2.*sinThetaVMa1*sinThetaVMa1r*cosPhiVMa1r)/
+	  (a1_8piDiv9_*(a1posMassTerm2_ + 8.)))*sinThetaVMa1*sinThetaVMa1r; // [2], formula (46)
+}
+
+double SVfitTauLikelihoodPolarization::compVMa1DecayProbT(
+	 double cosThetaVMa1, double sinThetaVMa1, 
+	 double cosThetaVMa1r, double sinThetaVMa1r, double cosPhiVMa1r, double sinPhiVMa1r) const
+{
+  return ((square(a1posMassTerm_*sinThetaVMa1*cosThetaVMa1r + 2.*cosThetaVMa1*sinThetaVMa1r*cosPhiVMa1r)
+          + 4.*square(sinThetaVMa1r)*square(sinPhiVMa1r))/
+	  (a1_16piDiv9_*(a1posMassTerm2_ + 8.)))*sinThetaVMa1*sinThetaVMa1r; // [2], formula (47)
+}
+
+double SVfitTauLikelihoodPolarization::probOneProngTwoPi0s(
          const pat::Tau& tau, const SVfitLegSolution& solution, double tauLeptonPol) const
 {
-  // dummy implementation
-  return 0.;
+//--- compute likelihood for tau- --> a1- nu --> pi- pi0 pi0 nu decay
+
+  std::cout << "<SVfitTauLikelihoodPolarization::probOneProngTwoPi0s>:" << std::endl;
+
+  double cosTheta = solution.cosThetaRest();
+  double theta = TMath::ACos(cosTheta);
+  double z = tau.energy()/solution.p4().energy();
+  
+  double probTauDecayL = (*a1LpolLineShape_)(theta, tauLeptonPol, z);
+  double probTauDecayT = (*a1TpolLineShape_)(theta, tauLeptonPol, z);
+  std::cout << " probTauDecayL = " << probTauDecayL << ", probTauDecayT = " << probTauDecayT << std::endl;
+
+//--- find "distinguishable" pion in tau-jet;
+//    in case "distinguishable" pion cannot be found 
+//   (e.g. in case "wrong" tau decay mode is reconstructed),
+//    assume the "distinguishable" pion to be very soft
+  const reco::Candidate* distPion = getDistPion(tau);
+  double xMeasured = ( distPion != 0 ) ? distPion->energy()/tau.energy() : 0.;
+  double thetaVMa1 = solution.thetaVMa1();
+  double cosThetaVMa1  = TMath::Cos(thetaVMa1);
+  double sinThetaVMa1  = TMath::Sin(thetaVMa1);
+  double thetaVMa1r = solution.thetaVMa1r();
+  double cosThetaVMa1r = TMath::Cos(thetaVMa1r);
+  double sinThetaVMa1r = TMath::Sin(thetaVMa1r);
+  double phiVMa1r = solution.phiVMa1r();
+  double cosPhiVMa1r   = TMath::Cos(phiVMa1r);
+  double sinPhiVMa1r   = TMath::Sin(phiVMa1r);
+  double xFitted = compVMa1x(cosThetaVMa1, sinThetaVMa1, cosThetaVMa1r, sinThetaVMa1r, cosPhiVMa1r);
+
+//--- CV: only non-collinear approximation type formulas available in literature
+//        for tau- --> a1- nu --> pi- pi0 pi0 nu decay
+  double probVMa1DecayL = compVMa1DecayProbL(cosThetaVMa1, sinThetaVMa1, cosThetaVMa1r, sinThetaVMa1r, cosPhiVMa1r);
+  double probVMa1DecayT = compVMa1DecayProbT(cosThetaVMa1, sinThetaVMa1, cosThetaVMa1r, sinThetaVMa1r, cosPhiVMa1r, sinPhiVMa1r);
+  std::cout << " probVMa1DecayL = " << probVMa1DecayL << ", probVMa1DecayT = " << probVMa1DecayT << std::endl;
+
+  double xSigma = xVMa1NeutralSigma_->Eval(tau.pt());
+  double xBias = xVMa1NeutralBias_->Eval(tau.pt());
+  std::cout << " xSigma = " << xSigma << ", xBias = " << xBias << std::endl;
+  double probSmear = TMath::Gaus(xMeasured, xFitted + xBias, xSigma);
+  std::cout << " probSmear = " << probSmear << std::endl;
+
+  double probL = probTauDecayL*probVMa1DecayL*probSmear;
+  double probT = probTauDecayT*probVMa1DecayT*probSmear;
+  std::cout << "--> probL = " << probL << ", probT = " << probT << std::endl;
+
+  return (probL + probT);
+}
+
+double SVfitTauLikelihoodPolarization::probThreeProngZeroPi0s(
+         const pat::Tau& tau, const SVfitLegSolution& solution, double tauLeptonPol) const
+{
+//--- compute likelihood for tau- --> a1- nu --> pi- pi+ pi- nu decay
+
+  std::cout << "<SVfitTauLikelihoodPolarization::probThreeProngZeroPi0s>:" << std::endl;
+
+  double cosTheta = solution.cosThetaRest();
+  double theta = TMath::ACos(cosTheta);
+  double z = tau.energy()/solution.p4().energy();
+  
+  double probTauDecayL = (*a1LpolLineShape_)(theta, tauLeptonPol, z);
+  double probTauDecayT = (*a1TpolLineShape_)(theta, tauLeptonPol, z);
+  std::cout << " probTauDecayL = " << probTauDecayL << ", probTauDecayT = " << probTauDecayT << std::endl;
+
+//--- find "distinguishable" pion in tau-jet;
+//    in case "distinguishable" pion cannot be found 
+//   (e.g. in case "wrong" tau decay mode is reconstructed),
+//    assume the "distinguishable" pion to be very soft
+  const reco::Candidate* distPion = getDistPion(tau);
+  double xMeasured = ( distPion != 0 ) ? distPion->energy()/tau.energy() : 0.;
+  double thetaVMa1 = solution.thetaVMa1();
+  double cosThetaVMa1  = TMath::Cos(thetaVMa1);
+  double sinThetaVMa1  = TMath::Sin(thetaVMa1);
+  double thetaVMa1r = solution.thetaVMa1r();
+  double cosThetaVMa1r = TMath::Cos(thetaVMa1r);
+  double sinThetaVMa1r = TMath::Sin(thetaVMa1r);
+  double phiVMa1r = solution.phiVMa1r();
+  double cosPhiVMa1r   = TMath::Cos(phiVMa1r);
+  double sinPhiVMa1r   = TMath::Sin(phiVMa1r);
+  double xFitted = compVMa1x(cosThetaVMa1, sinThetaVMa1, cosThetaVMa1r, sinThetaVMa1r, cosPhiVMa1r);
+
+//--- CV: only non-collinear approximation type formulas available in literature
+//        for tau- --> a1- nu --> pi- pi+ pi- nu decay
+  double probVMa1DecayL = compVMa1DecayProbL(cosThetaVMa1, sinThetaVMa1, cosThetaVMa1r, sinThetaVMa1r, cosPhiVMa1r);
+  double probVMa1DecayT = compVMa1DecayProbT(cosThetaVMa1, sinThetaVMa1, cosThetaVMa1r, sinThetaVMa1r, cosPhiVMa1r, sinPhiVMa1r);
+  std::cout << " probVMa1DecayL = " << probVMa1DecayL << ", probVMa1DecayT = " << probVMa1DecayT << std::endl;
+
+  double xSigma = xVMa1ChargedSigma_->Eval(tau.pt());
+  double xBias = xVMa1ChargedBias_->Eval(tau.pt());
+  std::cout << " xSigma = " << xSigma << ", xBias = " << xBias << std::endl;
+  double probSmear = TMath::Gaus(xMeasured, xFitted + xBias, xSigma);
+  std::cout << " probSmear = " << probSmear << std::endl;
+
+  double probL = probTauDecayL*probVMa1DecayL*probSmear;
+  double probT = probTauDecayT*probVMa1DecayT*probSmear;
+  std::cout << "--> probL = " << probL << ", probT = " << probT << std::endl;
+
+  return (probL + probT);
 }
 
 double SVfitTauLikelihoodPolarization::probOtherDecayMode(
--- plugins/SVfitTauLikelihoodPolarization.h
+++ plugins/SVfitTauLikelihoodPolarization.h
@@ -13,9 +13,9 @@
  *
  * \author Christian Veelken, UC Davis
  *
- * \version $Revision: 1.1 $
+ * \version $Revision: 1.2 $
  *
- * $Id: SVfitTauLikelihoodPolarization.h,v 1.1 2010/09/06 07:48:16 veelken Exp $
+ * $Id: SVfitTauLikelihoodPolarization.h,v 1.2 2010/09/06 12:08:08 veelken Exp $
  *
  */
 
@@ -24,6 +24,9 @@
 #include "DataFormats/PatCandidates/interface/Tau.h"
 
 #include "TauAnalysis/CandidateTools/interface/SVfitLegLikelihoodPolarizationBase.h"
+#include "TauAnalysis/CandidateTools/interface/SVfitVMlineShapeIntegral.h"
+
+#include <TFormula.h>
 
 class SVfitTauLikelihoodPolarization : public SVfitLegLikelihoodPolarizationBase<pat::Tau>
 {
@@ -34,11 +37,17 @@ class SVfitTauLikelihoodPolarization : public SVfitLegLikelihoodPolarizationBase
  private:
   double negLogLikelihoodPolarized(const pat::Tau&, const SVfitLegSolution&, double) const;
 
-  double probOneProngZeroPi0(const pat::Tau&, const SVfitLegSolution&, double) const;
+  double probOneProngZeroPi0s(const pat::Tau&, const SVfitLegSolution&, double) const;
   double probOneProngOnePi0(const pat::Tau&, const SVfitLegSolution&, double) const;
-  double probOneProngTwoPi0(const pat::Tau&, const SVfitLegSolution&, double) const;
-  double probThreeProngZeroPi0(const pat::Tau&, const SVfitLegSolution&, double) const;
+  double probOneProngTwoPi0s(const pat::Tau&, const SVfitLegSolution&, double) const;
+  double probThreeProngZeroPi0s(const pat::Tau&, const SVfitLegSolution&, double) const;
   double probOtherDecayMode(const pat::Tau&, const SVfitLegSolution&, double) const;
+
+//--- auxiliary functions needed for computation of likelihood 
+//    for tau- --> a1- nu --> pi- pi0 pi0 nu, tau- --> a1- nu --> pi- pi+ pi- nu decays 
+  double compVMa1x(double, double, double, double, double) const;
+  double compVMa1DecayProbL(double, double, double, double, double) const;
+  double compVMa1DecayProbT(double, double, double, double, double, double) const;
 
   std::vector<int> supportedTauDecayModes_;
   size_t numSupportedTauDecayModes_;
@@ -48,11 +57,32 @@ class SVfitTauLikelihoodPolarization : public SVfitLegLikelihoodPolarizationBase
   mutable TVectorD vGen_;
   mutable TVectorD vProb_;
 
+  TFormula* xVMrhoSigma_;
+  TFormula* xVMrhoBias_;
+  TFormula* xVMa1NeutralSigma_;
+  TFormula* xVMa1NeutralBias_;
+  TFormula* xVMa1ChargedSigma_;
+  TFormula* xVMa1ChargedBias_;
+
+  SVfitVMlineShapeIntegral* rhoLpolLineShape_;
+  SVfitVMlineShapeIntegral* rhoTpolLineShape_;
+  SVfitVMlineShapeIntegral* a1LpolLineShape_;
+  SVfitVMlineShapeIntegral* a1TpolLineShape_;
+
   int tauDecayModeOther_index_;
 
   SVfitLegLikelihoodBase<pat::Tau>* likelihoodPhaseSpace_;
 
   bool useCollApproxFormulas_;
+
+//--- temporary variables to speed-up computations
+//    (computed once in constructor)
+  double a1posMassTerm_;
+  double a1posMassTerm2_;
+  double a1negMassTerm_;
+  double a1q_;
+  double a1_8piDiv9_;
+  double a1_16piDiv9_;
 };
 
 #endif
