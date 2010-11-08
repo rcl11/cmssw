b40fbd4d9571dddfdeb7ebd71845a28d3acb9cff
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-11-08 10:00:32 +0000

    Add support for selected fit iterations

--- python/svFitAlgorithm_cfi.py
+++ python/svFitAlgorithm_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 svFitLikelihoodDiTauKinematicsPhaseSpace = cms.PSet(
     pluginName = cms.string("svFitLikelihoodDiTauKinematicsPhaseSpace"),
     pluginType = cms.string("SVfitLikelihoodDiTauLegs"),
+    # Always fit
+    firstFitIteration = cms.uint32(0),
     leg1 = cms.PSet(
         pluginType = cms.string("SVfitLegLikelihoodPhaseSpace")
     ),
@@ -14,6 +16,8 @@ svFitLikelihoodDiTauKinematicsPhaseSpace = cms.PSet(
 svFitLikelihoodDiTauKinematicsPolarized = cms.PSet(
     pluginName = cms.string("svFitLikelihoodDiTauKinematicsPolarized"),
     pluginType = cms.string("SVfitLikelihoodDiTauLegs"),
+    # Always fit
+    firstFitIteration = cms.uint32(0),
     leg1 = cms.PSet(
         pluginType = cms.string("SVfitLegLikelihoodPolarizationBase"),
         usePolarization = cms.bool(True),
@@ -55,6 +59,8 @@ svFitLikelihoodDiTauKinematicsPolarized = cms.PSet(
 svFitLikelihoodMEt = cms.PSet(
     pluginName = cms.string("svFitLikelihoodMEt"),
     pluginType = cms.string("SVfitLikelihoodMEt"),
+    # Always fit
+    firstFitIteration = cms.uint32(0),
     resolution = cms.PSet(
         parSigma = cms.string("2.6 + 0.0383*x"),
         parBias = cms.string("1.45"),
@@ -67,6 +73,8 @@ svFitLikelihoodMEt = cms.PSet(
 svFitLikelihoodTrackInfo = cms.PSet(
     pluginName = cms.string("svFitLikelihoodTrackInfo"),
     pluginType = cms.string("SVfitLikelihoodDiTauTrackInfo"),
+    # Only fit on second iteration
+    firstFitIteration = cms.uint32(1),
     leg1 = cms.PSet(
         pluginType = cms.string("SVfitLegLikelihoodTrackInfo"),
         useLinearApprox = cms.bool(True)
@@ -74,18 +82,23 @@ svFitLikelihoodTrackInfo = cms.PSet(
     leg2 = cms.PSet(
         pluginType = cms.string("SVfitLegLikelihoodTrackInfo"),
         useLinearApprox = cms.bool(True)
-    ),    
+    ),
+
     useLifetimeConstraint = cms.bool(True)
 )
 
 svFitLikelihoodDiTauPtBalance = cms.PSet(
     pluginName = cms.string("svFitLikelihoodDiTauPtBalance"),
-    pluginType = cms.string("SVfitLikelihoodDiTauPtBalance")
+    pluginType = cms.string("SVfitLikelihoodDiTauPtBalance"),
+    # Always fit
+    firstFitIteration = cms.uint32(0),
 )
 
 svFitLikelihoodDiTauPt = cms.PSet(
     pluginName = cms.string("svFitLikelihoodDiTauPt"),
     pluginType = cms.string("SVfitLikelihoodDiTauPt"),
+    # Always fit
+    firstFitIteration = cms.uint32(0),
     pdf = cms.string("[0]*[1]/[2]*TMath::Sqrt(0.5*TMath::Pi())*TMath::Exp([1]*[1]/(2*[2]*[2]) - (x - [3])/[2])"
                     + "*(1. - TMath::Erf((([3] - x)/[1] + [1]/[2])/TMath::Sqrt(2.)))"),
     par0 = cms.string("5.38553e-1"),
@@ -97,6 +110,8 @@ svFitLikelihoodDiTauPt = cms.PSet(
 svFitLikelihoodDiTauProdZ0 = cms.PSet(
     pluginName = cms.string("svFitLikelihoodDiTauProdZ0"),
     pluginType = cms.string("SVfitLikelihoodDiTauProd"),
+    # Always fit
+    firstFitIteration = cms.uint32(0),
     process = cms.string("Z0"), # either 'Z0' or 'Higgs'
     pdfSet = cms.string("cteq65.LHgrid"),
     sqrtS = cms.double(7000.) # 7 TeV center-of-mass energy
