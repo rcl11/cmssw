9b62b3ce18dba7cd2e08817fc90e61baf49d6d08
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-10-06 10:40:53 +0000

    SVfit plugin vs standalone version test files for all lepton types

--- test/testNSVfitEvent_mutau_cfg.py
+++ test/testNSVfitEvent_mutau_cfg.py
@@ -7,19 +7,13 @@ process.source = cms.Source("PoolSource",
     "file:patTuple.root"
   )
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(102) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.MessageLogger = cms.Service("MessageLogger")
 
 process.TFileService = cms.Service("TFileService",
   fileName = cms.string('analyzeNSVfit.root')
 )
-
-## load nSVfitEventAnalyzer
-process.load("TauAnalysis.CandidateTools.nSVfitEventAnalyzer_cfi")
-
-## load nSVfitProducer (plugin version)
-process.load("TauAnalysis.CandidateTools.nSVfitAlgorithmDiTau_cfi")
 
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.Geometry_cff")
@@ -28,12 +22,27 @@ from Configuration.PyReleaseValidation.autoCond import autoCond
 process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
+
+## load nSVfitProducer (plugin version)
+process.load("TauAnalysis.CandidateTools.nSVfitAlgorithmDiTau_cfi")
+
 process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg1.src = "cleanPatMuons"
-process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg2.src = "cleanPatElectrons"
-process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg2.likelihoodFunctions = cms.VPSet(process.nSVfitElectronLikelihoodPhaseSpace)
-process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg2.builder = process.nSVfitTauToElecBuilder
+process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg1.likelihoodFunctions = cms.VPSet(process.nSVfitMuonLikelihoodPhaseSpace)
+process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg1.builder = process.nSVfitTauToMuonBuilder
+
+
+process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg2.src = "cleanPatTaus"
+process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg2.likelihoodFunctions = cms.VPSet(process.nSVfitTauLikelihoodPhaseSpace)
+process.nSVfitProducerByLikelihoodMaximization.config.event.resonances.A.daughters.leg2.builder = process.nSVfitTauToHadBuilder
+
 process.nSVfitProducerByLikelihoodMaximization.config.event.srcMEt = "patMETs"
 process.nSVfitProducerByLikelihoodMaximization.algorithm.verbosity = 0
+
+
+## load nSVfitEventAnalyzer
+process.load("TauAnalysis.CandidateTools.nSVfitEventAnalyzer_cfi")
+process.nSVfitEventAnalyzer.leps1 = "cleanPatMuons"
+process.nSVfitEventAnalyzer.leps2 = "cleanPatTaus"
 
 ## check the event content
 process.content = cms.EDAnalyzer("EventContentAnalyzer")
