9d9cc125ac93cc4e1757ba81d379f3a3beb5ccf3
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-10-31 11:25:03 +0000

    made mass penalty term configurable ( allow to use log(eff @ mass) instead of log(mass) )

--- plugins/NSVfitResonanceLikelihoodMassPenalty.cc
+++ plugins/NSVfitResonanceLikelihoodMassPenalty.cc
@@ -1,20 +1,39 @@
 #include "TauAnalysis/CandidateTools/plugins/NSVfitResonanceLikelihoodMassPenalty.h"
-#include "TMath.h"
 
-NSVfitResonanceLikelihoodMassPenalty::NSVfitResonanceLikelihoodMassPenalty(
-    const edm::ParameterSet& pset):NSVfitResonanceLikelihood(pset) {
-  power_ = pset.getParameter<double>("power");
+#include <TMath.h>
+#include <TString.h>
+
+#include <string>
+
+NSVfitResonanceLikelihoodMassPenalty::NSVfitResonanceLikelihoodMassPenalty(const edm::ParameterSet& cfg)
+  : NSVfitResonanceLikelihood(cfg)
+{
+  TString formula_string = cfg.getParameter<std::string>("formula").data();
+  formula_string.ReplaceAll("mass", "x");
+  
+  formula_ = new TFormula(TString(pluginName_.data()).Append("_formula").Data(), formula_string.Data());
+
+  power_ = cfg.getParameter<double>("power");
 }
 
-double NSVfitResonanceLikelihoodMassPenalty::operator()(
-    const NSVfitResonanceHypothesis* resonance) const {
+NSVfitResonanceLikelihoodMassPenalty::~NSVfitResonanceLikelihoodMassPenalty()
+{
+  delete formula_;
+}
+
+double NSVfitResonanceLikelihoodMassPenalty::operator()(const NSVfitResonanceHypothesis* resonance) const 
+{
   assert(resonance);
+
   double mass = resonance->p4_fitted().mass();
-  //std::cout << "<LogM Term> " << std::endl
-  //	      << "m = " << mass << "  ||  power*logM = " << power_*TMath::Log(mass) << std::endl;
-  return power_*TMath::Log(mass);
+
+  double retVal = power_*formula_->Eval(mass);
+  std::cout << "<NSVfitResonanceLikelihoodMassPenalty::operator()>:" << std::endl;
+  std::cout << " formula = " << formula_->GetTitle() << ": mass = " << mass << " --> retVal = " << retVal << std::endl;
+  
+  return retVal;
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_EDM_PLUGIN(NSVfitResonanceLikelihoodPluginFactory,
-    NSVfitResonanceLikelihoodMassPenalty, "NSVfitResonanceLikelihoodMassPenalty");
+
+DEFINE_EDM_PLUGIN(NSVfitResonanceLikelihoodPluginFactory, NSVfitResonanceLikelihoodMassPenalty, "NSVfitResonanceLikelihoodMassPenalty");
--- plugins/NSVfitResonanceLikelihoodMassPenalty.h
+++ plugins/NSVfitResonanceLikelihoodMassPenalty.h
@@ -3,10 +3,17 @@
 
 /** \class NSVfitResonanceLikelihoodMassPenalty
  *
- * Adds a penalty term for high masses.  The return value of operator()
- * is
+ * Adds a penalty term for high masses.  
+ * The return value of operator() is configurable via python.
  *
- * -penaltyFactor_*Log(resonanceMass)
+ * Options used in the past are:
+ *  o TMath::Log(mass)
+ *  o [0]*([1] - TMath::Erf((x - [2])*[3]))
+ *    with p0 = 4.21e-2, p1 = 2.52e-2, p2 = 4.40e+1, p3 = -6.90e-3 
+ *   (efficiency of gg --> Higgs --> mu + tau_had channel in 2010 analysis)
+ *  o [0]*([1] - TMath::Erf((x - [2])*[3]))
+ *    with p0 = 2.49e-2, p1 = 7.78e-2, p2 = 5.63e+1, p3 = -7.53e-3 
+ *   (efficiency of gg --> Higgs --> e + tau_had channel in 2010 analysis)
  *
  * \author Evan Friis, Christian Veelken; UC Davis
  *
@@ -18,11 +25,14 @@
 
 #include "AnalysisDataFormats/TauAnalysis/interface/NSVfitResonanceHypothesis.h"
 
+#include "TFormula.h"
+
 class NSVfitResonanceLikelihoodMassPenalty : public NSVfitResonanceLikelihood
 {
  public:
+
   NSVfitResonanceLikelihoodMassPenalty(const edm::ParameterSet&);
-  ~NSVfitResonanceLikelihoodMassPenalty() {};
+  ~NSVfitResonanceLikelihoodMassPenalty();
 
   void beginJob(NSVfitAlgorithmBase*) const {}
 
@@ -31,6 +41,9 @@ class NSVfitResonanceLikelihoodMassPenalty : public NSVfitResonanceLikelihood
   double operator()(const NSVfitResonanceHypothesis*) const;
 
  private:
+
+  TFormula* formula_;
+
   double power_;
 };
 
